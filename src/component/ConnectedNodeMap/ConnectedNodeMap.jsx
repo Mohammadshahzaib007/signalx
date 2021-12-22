27a567c70d6503676480dac11e934cae7196e2bc
MohammadShahzaib007 <shahzaibmohammad007@gmail.com>
2021-12-22 17:27:21 +0530

    Added icons

--- src/component/ConnectedNodeMap/ConnectedNodeMap.jsx
+++ src/component/ConnectedNodeMap/ConnectedNodeMap.jsx
@@ -16,11 +16,11 @@ function ConnectedNodeMap(props) {
 
     const isAddedNodes = [];
     data.forEach(item => {
-        nodes.push({ ...item.organization, icon: companyIcon })
+        nodes.push({ ...item.organization, icon: 'companyIcon' })
 
         item.members.forEach(e => {
             if (!isAddedNodes.includes(e.din)) {
-                nodes.push({ ...e, icon: userIcon })
+                nodes.push({ ...e, icon: 'userIcon' })
 
             }
 
@@ -42,7 +42,7 @@ function ConnectedNodeMap(props) {
 
 
     const graphData = {
-        nodes: nodes.map(({ din, cin, name }) => ({ id: din ?? cin, name })),
+        nodes: nodes.map(({ din, cin, name, icon }) => ({ id: din ?? cin, name, icon })),
         links: [...links]
     }
 
@@ -55,17 +55,33 @@ function ConnectedNodeMap(props) {
             <ForceGraph2D
                 graphData={graphData}
                 nodeLabel=""
+                nodeColor="white"
+                nodeAutoColorBy="#fff"
                 nodeCanvasObjectMode={() => "after"}
                 nodeCanvasObject={(node, ctx, globalScale) => {
                     const label = node.name;
-                    const fontSize = 10 / globalScale;
+                    const fontSize =8 / globalScale;
                     ctx.font = `${fontSize}px Sans-Serif`;
                     ctx.textAlign = "center";
                     ctx.textBaseline = "middle";
                     ctx.fillStyle = "black"; //node.color;
-                    ctx.fillText(label, node.x, node.y + 8);
+                    ctx.fillText(label, node.x, node.y + 10);
+
+                    const size = 15;
+                    const img = new Image();
+
+                    const src = node.icon === "companyIcon" ? companyIcon : userIcon;
+
+                    img.src = src;
+                    ctx.drawImage(img,
+                        node.x - size / 2 + 0,
+                        node.y - size / 2 + 1,
+                        size + 1,
+                        size - 1
+                    )
+                    return ctx
                 }}
-                minZoom={3}
+                minZoom={2}
                 linkWidth={2}
             // autoPauseRedraw={false}
 
